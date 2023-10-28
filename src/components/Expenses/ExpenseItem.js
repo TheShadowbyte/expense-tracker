ea1a61ba8df312bcc2beec3e1588653db10a07cf
Dimitar Jordanov <theshadowbyte@gmail.com>
2023-10-28 19:02:38 -0400

    Removed the title update functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 
 function ExpenseItem(props) {
 
-    const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
-                <button onClick={clickHandler}>Change Title</button>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </Card>
