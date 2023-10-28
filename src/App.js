c0b567efd65506dff445bd85d765e624bfc38726
Dimitar Jordanov <theshadowbyte@gmail.com>
2023-10-28 19:45:59 -0400

    Moved the expenses array from Expenses.js to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
+    const expenses = [
+        { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+        { id: 'e2', title: 'Toilet Paper', amount: 12.94, date: new Date(2022, 5, 12) },
+        { id: 'e3', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 28) },
+        { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2024, 5, 12) }
+    ];
+
     const addExpenseHandler = expense => {
         console.log('In App.js');
         console.log(expense);
@@ -12,7 +19,7 @@ function App() {
     return (
         <div className="App">
             <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses />
+            <Expenses items={expenses} />
         </div>
     );
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,14 +4,7 @@ import Card from "./Card";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses() {
-
-    const expenses = [
-        { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-        { id: 'e2', title: 'Toilet Paper', amount: 12.94, date: new Date(2022, 5, 12) },
-        { id: 'e3', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 28) },
-        { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2024, 5, 12) }
-    ];
+function Expenses(props) {
 
     const listOfYears = ['2022', '2021', '2020', '2019'];
     const [filteredYear, setFilteredYear] = useState('All');
@@ -48,7 +41,7 @@ function Expenses() {
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterExpenses={filterExpensesHandler} />
             <p>{filterInfoText}</p>
-            {expenses.map(expense => (
+            {props.items.map(expense => (
                 <ExpenseItem
                     key={expense.id}
                     title={expense.title}
