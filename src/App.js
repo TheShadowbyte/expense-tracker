53fc67558e8355cb30cb6db1fa6acd692ce0e5c3
Dimitar Jordanov <theshadowbyte@gmail.com>
2023-10-28 22:58:21 -0400

    Move the static expenses out of the App function and make the expenses stateful.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const staticExpenses = [
+    { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: 'e2', title: 'Toilet Paper', amount: 12.94, date: new Date(2022, 5, 12) },
+    { id: 'e3', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 28) },
+    { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2024, 5, 12) }
+];
 
-    const expenses = [
-        { id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-        { id: 'e2', title: 'Toilet Paper', amount: 12.94, date: new Date(2022, 5, 12) },
-        { id: 'e3', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 28) },
-        { id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2024, 5, 12) }
-    ];
+const App = () => {
+
+    const [expenses, setExpenses] = useState(staticExpenses);
 
     const addExpenseHandler = expense => {
-        console.log('In App.js');
-        console.log(expense);
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     };
 
     return (
