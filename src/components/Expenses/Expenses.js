f971307cfab3f65c5e1b8e1f864dd273bdbd613c
Dimitar Jordanov <theshadowbyte@gmail.com>
2023-10-31 12:59:36 -0400

    Added a check for "no expenses found"

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,34 +8,44 @@ const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('All');
 
-    let filterInfoText = '';
-    let filteredItemsArray = [];
+    let filterInfoText;
+    let filteredExpenses;
+    let expensesContent = <p>No expenses found</p>;
 
     if (filteredYear !== 'All') {
-        filterInfoText = 'Data for ' + filteredYear + ' is shown.';
-        filteredItemsArray = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+        filterInfoText = <p>EXPENSES FOR {filteredYear} </p>;
+        filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
     }
     else {
-        filterInfoText = 'Data for all years is shown.';
-        filteredItemsArray = props.items;
+        filterInfoText = <p>EXPENSES FOR ALL YEARS</p>;
+        filteredExpenses = props.items;
     }
 
     const filterExpensesHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
 
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onFilterExpenses={filterExpensesHandler} />
-            <p>{filterInfoText}</p>
-            {filteredItemsArray.map(expense => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
+            <ExpensesFilter
+                selected={filteredYear}
+                onFilterExpenses={filterExpensesHandler}
+            />
+
+            {filterInfoText}
+            {expensesContent}
+
         </Card>
     );
 
